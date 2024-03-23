8eb14fc0fd5366c950b1214374ed313ee88182a9
zhuo hu <16635905985@163.com>
2024-03-24 02:50:28 +0800

    use pytest form python3 text code

--- python/test_calculator.py
+++ python/test_calculator.py
@@ -2,32 +2,18 @@ from calculator import square
 
 
 def main():
-    test_square()
+    test_assert()
 
 
-def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 squared is not 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 squared is not 9")
-    try:
-        assert square(-2) == 4
-    except AssertionError:
-        print("-2 squared is not 4")
-    try:
-        assert square(-3) == 9
-    except AssertionError:
-        print("-3 squared is not 9")
-    try:
-        assert square(0) == 0
-    except AssertionError:
-        print("0 squared is not 0")
+from calculator import square
 
+
+def test_assert():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
 
 if __name__ == "__main__":
     main()
-
