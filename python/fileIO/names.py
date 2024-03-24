377c797717c83684f1b3cd0b49eb7d8071a3abd4
zhuo hu <16635905985@163.com>
2024-03-24 13:12:03 +0800

    three names

--- python/fileIO/names.py
+++ python/fileIO/names.py
@@ -1,2 +1,6 @@
-name=input("what's your name?")
-print(f"hello,{name}")
+names=[]
+
+for _ in range(3):
+    names.append(input("what's your name:"))
+for name in sorted(names):
+    print(f"hello, {name}")
