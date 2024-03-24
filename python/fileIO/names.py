e57d29f451f4ebd558828516c278d442ac593c8d
zhuo hu <16635905985@163.com>
2024-03-24 13:26:47 +0800

    file open,writen,etc

--- python/fileIO/names.py
+++ python/fileIO/names.py
@@ -1,6 +1,5 @@
-names=[]
+name =input("what's your name?")
 
-for _ in range(3):
-    names.append(input("what's your name:"))
-for name in sorted(names):
-    print(f"hello, {name}")
+file= open("names.txt","a")
+file.write(f"{name}\n")
+file.close
