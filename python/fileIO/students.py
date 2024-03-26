dec1a71dec2df2a0e09eb7984d28542fa92185e5
zhuo hu <16635905985@163.com>
2024-03-27 00:53:03 +0800

    deal with csv using python

--- python/fileIO/students.py
+++ python/fileIO/students.py
@@ -1,4 +1,5 @@
 with open("students.csv") as file:
     for line in file:
-        row=line.rstrip().split(",")
-        print(f"{row[0]} in {row[1]}")
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
+
