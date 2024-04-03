b2238c054b3d62a4e9202c406324614c46df3dc4
zhuo hu <16635905985@163.com>
2024-04-03 20:05:50 +0800

    c plus plus study

--- cplus/text.cpp
+++ cplus/text.cpp
@@ -1,24 +1,26 @@
-#include<iostream>
-#include<cstring>
-
+#include <iostream>
+#include <cstring>
+ 
 using namespace std;
-// namespace name
-int main()
+ 
+int main ()
 {
-    string str1="RJSFVJS";
-    string str2="Google";
-    string str3;
-    int len;
-
-    str3=str1;
-    cout<<"str3:"<<str3<<endl;
-
-    str3=str1+str2;
-    cout<<"str1+str2:"<<str3<<endl;
-
-    len=str3.size();
-    cout<<"the len of str3:"<<len<<endl;
-
-
+   char str1[13] = "runoob";
+   char str2[13] = "google";
+   char str3[13];
+   int  len ;
+ 
+   // 复制 str1 到 str3
+   strcpy( str3, str1);
+   cout << "strcpy( str3, str1) : " << str3 << endl;
+ 
+   // 连接 str1 和 str2
+   strcat( str1, str2);
+   cout << "strcat( str1, str2): " << str1 << endl;
+ 
+   // 连接后，str1 的总长度
+   len = strlen(str1);
+   cout << "strlen(str1) : " << len << endl;
+ 
+   return 0;
 }
-
