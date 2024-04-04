014ce7288db12ff6dedf37c207ffcf1d07f8dba6
zhuo hu <16635905985@163.com>
2024-04-04 17:45:22 +0800

    c plus plus study

--- cplus/text.cpp
+++ cplus/text.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
  
-#define PI 3.14159
- 
 int main ()
 {
- 
-    cout << "Value of PI :" << PI << endl; 
+    cout << "Value of __LINE__ : " << __LINE__ << endl;
+    cout << "Value of __FILE__ : " << __FILE__ << endl;
+    cout << "Value of __DATE__ : " << __DATE__ << endl;
+    cout << "Value of __TIME__ : " << __TIME__ << endl;
  
     return 0;
 }
