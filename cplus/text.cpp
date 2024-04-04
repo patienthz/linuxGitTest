a9d938da1e96eda23b45c432c2ec17e77954eb11
zhuo hu <16635905985@163.com>
2024-04-04 09:37:12 +0800

    c plus plus study

--- cplus/text.cpp
+++ cplus/text.cpp
@@ -1,27 +1,45 @@
 #include <iostream>
+ 
+using namespace std;
 
-class MyClass {
-public:
-    int data;
-
-    void display() {
-        std::cout << "Data: " << data << std::endl;
-    }
+class Box
+{
+   public:
+      // 构造函数定义
+      Box(double l=2.0, double b=2.0, double h=2.0)
+      {
+         cout <<"Constructor called." << endl;
+         length = l;
+         breadth = b;
+         height = h;
+      }
+      double Volume()
+      {
+         return length * breadth * height;
+      }
+   private:
+      double length;     // Length of a box
+      double breadth;    // Breadth of a box
+      double height;     // Height of a box
 };
 
-int main() {
-    // 创建类对象
-    MyClass obj;
-    obj.data = 42;
+int main(void)
+{
+   Box Box1(3.3, 1.2, 1.5);    // Declare box1
+   Box Box2(8.5, 6.0, 2.0);    // Declare box2
+   Box *ptrBox;                // Declare pointer to a class.
 
-    // 声明和初始化指向类的指针
-    MyClass *ptr = &obj;
+   // 保存第一个对象的地址
+   ptrBox = &Box1;
 
-    // 通过指针访问成员变量
-    std::cout << "Data via pointer: " << ptr->data << std::endl;
+   // 现在尝试使用成员访问运算符来访问成员
+   cout << "Volume of Box1: " << ptrBox->Volume() << endl;
 
-    // 通过指针调用成员函数
-    ptr->display();
+   // 保存第二个对象的地址
+   ptrBox = &Box2;
 
-    return 0;
+   // 现在尝试使用成员访问运算符来访问成员
+   cout << "Volume of Box2: " << ptrBox->Volume() << endl;
+ 
+   return 0;
 }
