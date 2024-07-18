63dd66603784c8494c98a6c06b7ed611aa28d965
zhuo hu <16635905985@163.com>
2024-07-18 10:10:56 +0800

    commit

---
+++ python/mysionna/channel/awgn.py
@@ -0,0 +1,8 @@
+
+
+class AWGN():
+    def __init__(self) -> None:
+        self.x=10
+    def text(x):
+        x=17
+        print(x)
---
+++ python/mysionna/channel/ofdm.py
@@ -0,0 +1,7 @@
+#import sys
+#sys.path.append('/home/zhuo/project_code/python')
+from mysionna.channel.awgn import AWGN
+
+
+awgn=AWGN()
+awgn.text()
